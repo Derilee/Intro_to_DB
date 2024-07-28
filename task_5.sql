9e1fe00286581fe6e2a3e69ec1717bf31d85b7e7
Derilee <victorsamuelpraise@gmail.com>
2024-07-28 23:26:57 +0100

    Populating Your Database with Your Very First Data

---
+++ task_5.sql
@@ -0,0 +1,5 @@
+USE alx_book_store;
+newrow = "INSERT INTO Customers(customer_id, customer_name, email, address) VALUES (%s, %s, %s, %s)"
+newvalue = ("1, Cole Baidoo, cbaidoo@sandtech.com, 123 Happiness Ave.")
+mycursor.execute(newrow, newvalue)
+mycursor.commit()

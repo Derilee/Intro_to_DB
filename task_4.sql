21cae22261db0cb50b7f1a5acd88ecb11c56e2d8
Derilee <victorsamuelpraise@gmail.com>
2024-07-28 23:14:19 +0100

    Full description

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,6 @@
 USE alx_book_store;
 
-mycursor.execute("INFORMATION_SCHEMA.COLUMNS", "COLUMN_NAME", "COLUMN_TYPE", "TABLE_SCHEMA = 'alx_book_store'", "TABLE_NAME = 'Books'")
+mycursor.execute("SELECT * FROM INFORMATION_SCHEMA.COLUMNS", "COLUMN_NAME", "COLUMN_TYPE", "TABLE_SCHEMA = 'alx_book_store'", "TABLE_NAME = 'Books'")
 description = mycursor.fetchall()
 
 for alldata in description:

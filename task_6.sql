61bf7c4a5166105f9ab372aeab8c66d2b75b7c26
Derilee <victorsamuelpraise@gmail.com>
2024-07-28 23:48:25 +0100

    Populating Your Database with Your More Data

--- task_6.sql
+++ task_6.sql
@@ -1,9 +1,8 @@
 USE alx_book_store;
-multiplevalue = (
-    ("2, Blessing Malik,bmalik @ sandtech.com, 124 Happiness Ave."),
-    ("3, Obed Ehoneah, eobed @ sandtech.com, 125 Happiness Ave."),
-    ("4, Nehemial Kamolu, nkamolu @ sandtech.com, 126 Happiness Ave.")
+multiplevalues = (
+    ("INSERT INTO customer", "VALUES", 2, 'Blessing Malik', "bmalik@sandtech.com", "124 Happiness  Ave."),
+    ("INSERT INTO customer", "VALUES", 3, 'Obed Ehoneah', 'eobed @ sandtech.com', '125 Happiness Ave.'),
+    ("INSERT INTO customer", "VALUES", 4, 'Nehemial Kamolu', 'nkamolu @ sandtech.com', '126 Happiness Ave.')
 )
-
-mycursor.execute(newrow, multiplevalue)
+mycursor.execute(newrow, multiplevalues)
 mycursor.commit()
